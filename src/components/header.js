2ef92b066b058070a02e40520f56a56f7020b723
adrielerodr <adriele.rodrigues@accenture.com>
2020-05-29 21:53:53 -0300

    New header

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,52 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const TabNavigation = ({ title, link}) => (
+  <h4 style={{ margin: 10, alignItems: 'center' }}>
+    <Link
+      to={link}
+      style={{
+        color: `white`,
+        textDecoration: `none`,
+      }}
+    >
+      { title }
+    </Link>
+  </h4>
+)
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#ea4646`,
       marginBottom: `1.45rem`,
+      padding: `1.45rem 1.0875rem`,
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'flex-end'
     }}
   >
-    <div
+    <h3 style={{ display: 'flex', alignItems: 'center', margin: 10 }}>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        { siteTitle }
+      </Link>
+    </h3>
+    <div 
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'space-around'
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <TabNavigation title="Blog" link="/blog" />
+      <TabNavigation title="About" link="/about" />
+      <TabNavigation title="Leituras" link="/reading" />
+      <TabNavigation title="Comunidades" link="/communities" />
     </div>
   </header>
 )
@@ -39,4 +60,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+export default Header;
