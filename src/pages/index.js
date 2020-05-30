4e073d9d848a5f4b03b17e4de02333b5e9aa5518
adrielerodr <adriele.rodrigues@accenture.com>
2020-05-29 21:05:45 -0300

    Add about text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Olá, eu sou Adriele Rodrigues!</h1>
+    <p>Eu sou desenvolvedora javascript na Accenture BR e atualmente moro em São Paulo.
+      Eu adoro desenvolver produtos novos e me admiro com lado artístico do design do front-end e da engenharia por trás da tela.
+      Gosto da loucura do mundo da tecnlogia e do desafio de aprender com cada evolução, além de trabalhar para o empoderamento feminino na área de TI.
+      Vamos aprender juntos?</p>
   </Layout>
 )
 
